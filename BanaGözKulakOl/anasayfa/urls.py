c3f892ac0ad8b2796e961d9e40bca3bf20da4f4b
canucan16 <121455493+canucan16@users.noreply.github.com>
2022-12-29 21:21:46 +0300

    Add files via upload

--- BanaGözKulakOl/anasayfa/urls.py
+++ BanaGözKulakOl/anasayfa/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include 
-from .views import IndexView, LogOutView, AboutUsView, ProfileView
+from .views import IndexView, LogOutView, AboutUsView, ProfileView, ProfileCustomize
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -8,8 +8,9 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("logoutsuccess/", LogOutView, name = 'log-out-success'),
     path("about/", AboutUsView, name = 'about-us-page'),
-    path("profile/", ProfileView, name = 'my-profile-page'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
+    path('profile/', ProfileView, name='profile-page'),
+    path ('profile/customize', ProfileCustomize, name='profile-customize-page')
 ]
