cf76f353064ce20dbde5835e482e5506dfdd5175
canucan16 <121455493+canucan16@users.noreply.github.com>
2023-01-01 22:11:25 +0300

    Delete BanaGözKulakOl directory

--- BanaGözKulakOl/anasayfa/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class nameSurnameForm(forms.Form):
-    your_name = forms.CharField(label='Adınız', max_length=100)
-    your_surname = forms.CharField(label='Soyadınız', max_length=100)
-    email = forms.CharField(label='E-Posta', max_length=100)
