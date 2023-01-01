311fd85396a5f89a4ca17a80d776dea9d9b4ac5b
canucan16 <121455493+canucan16@users.noreply.github.com>
2023-01-01 22:13:14 +0300

    Add files via upload

---
+++ BanaGözKulakOl/anasayfa/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class nameSurnameForm(forms.Form):
+    your_name = forms.CharField(label='Adınız', max_length=100)
+    your_surname = forms.CharField(label='Soyadınız', max_length=100)
+    email = forms.CharField(label='E-Posta', max_length=100)
+    username = forms.CharField(label='Kullanıcı Adı', max_length=100)
